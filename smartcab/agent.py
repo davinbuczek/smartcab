8b46f65bfe9bfd05e6a7638799076616be0c4e2a
davinbuczek <davinbuczek@me.com>
2016-09-06 16:51:29 -0700

    Improve the Q-Learning Driving Agent
    agent.py: added comments and functionality to print trial number,
    status, deadline and steps to excel file to tune parameters for best
    performance of the algorithm
    
    environment.py: changed temporary hooks to reference new summary method
    for analysis purposes

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -1,4 +1,5 @@
 import random
+import csv
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -12,10 +13,15 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.trial = 0
-        self.valid_waypoints = ['left','forward','right']
-        self.valid_lights = ['red', 'green']
-        self.valid_actions = [None, 'forward', 'left', 'right']
-        self.Q = {}
+        self.steps = 0
+        self.deadline = 0
+        self.alpha = 0.5
+        self.gamma = 0.2
+        self.data_list = [["Trial", "Status", "Deadline", "Steps"]] # lists of lists for capturing data from trial rums for performance tuning
+        self.valid_waypoints = ['left','forward','right'] # list of valid directions based on suggested waypoint
+        self.valid_lights = ['red', 'green'] # list of valid traffic light states
+        self.valid_actions = [None, 'forward', 'left', 'right'] # list of valid actions the smartcab can take
+        self.Q = {} # initialize Q dictionary for chosen evniornment states to random values
         for waypoint in self.valid_waypoints:
             for light in self.valid_lights:
                 for oncoming in self.valid_actions:
@@ -24,18 +30,18 @@ class LearningAgent(Agent):
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.deadline = self.env.agent_states[self.env.primary_agent]['deadline']
+        self.steps = 0
         self.trial += 1
         
+    def summary(self, status):
+        self.data_list.append([self.trial, status, self.deadline, self.steps])
 
-    def success(self):
-        print "{} Success!".format(self.trial)
-        print self.env.agent_states[self.env.primary_agent]
-
-    def fail(self):
-        print "{} Fail".format(self.trial)
-        
-    def abort(self):
-        print "{} Abort".format(self.trial)
+        if self.trial == 100: # once test complete write data_list struct to csv file
+            filename = "alpha{}_gamma{}.csv".format(self.alpha, self.gamma)
+            with open(filename, 'wb') as csvfile:
+                writer = csv.writer(csvfile, dialect='excel')
+                writer.writerows(self.data_list)
 
     def update(self, t):
         # Gather inputs
@@ -45,32 +51,28 @@ class LearningAgent(Agent):
 
         # TODO: Update state
         self.state = (self.next_waypoint, inputs['light'], inputs['oncoming'])
+        self.steps += 1
 
         # TODO: Select action according to your policy
-        
-
-        Qvalue = self.Q[self.state]
-        best_value = max(Qvalue)
-        best_index = Qvalue.index(best_value)
-        action = self.valid_actions[best_index]
+        Qvalue = self.Q[self.state] # assign Qvalue to current state
+        best_value = max(Qvalue) # assign best_value to the max value of Qvalue
+        best_index = Qvalue.index(best_value) # assign best_index to the index of the best_value
+        action = self.valid_actions[best_index] # assign action to the value of best index
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 		
         # TODO: Learn policy based on state, action, reward
-        gamma = 0.2
-        alpha = 0.5
 
         inputs = self.env.sense(self)
 
         nextMax = max(self.Q[(self.planner.next_waypoint(), inputs['light'], inputs['oncoming'])])
 
-        self.Q[self.state][best_index] = (1 - alpha) * best_value + (alpha * (reward + gamma * nextMax))
+        self.Q[self.state][best_index] = (1 - self.alpha) * best_value + (self.alpha * (reward + self.gamma * nextMax))
 
-        
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}"\
         #.format(deadline, inputs, action, reward)  # [debug]
-		
+
 	def return_state(self):
 		return self.state
 
@@ -84,7 +86,7 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.0, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
